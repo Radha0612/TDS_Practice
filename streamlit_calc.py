8642c15c9f2b8db401de6d6b4a5d459c5c1445c7
Radha0612 <167615112+Radha0612@users.noreply.github.com>
2024-04-20 17:18:42 +0530

    Create streamlit_calc.py

---
+++ streamlit_calc.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def addition(a,b)
+  return(a+b)
+
+st.title("Little Calculator for addition")
+a=st.number_input("Enter the first number:")
+b=st.number_input("Enter the second number:")
+
+add = addition(a+b)
+
+st.write("The sum of two numbers is :",add)

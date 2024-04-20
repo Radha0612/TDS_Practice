f0aa0564655518c9231895827c4756d1ac21271b
Radha0612 <167615112+Radha0612@users.noreply.github.com>
2024-04-20 17:25:58 +0530

    Update streamlit_calc.py

--- streamlit_calc.py
+++ streamlit_calc.py
@@ -7,6 +7,6 @@ st.title("Little Calculator for addition")
 a=st.number_input("Enter the first number:")
 b=st.number_input("Enter the second number:")
 
-add = addition(a+b)
+add = addition(a,b)
 
 st.write("The sum of two numbers is :",add)

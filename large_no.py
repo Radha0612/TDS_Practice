6b8e56b6fac4f48dbd41897e18ea8c9a9a7527f2
Radha0612 <167615112+Radha0612@users.noreply.github.com>
2024-04-20 17:47:25 +0530

    Update large_no.py

--- large_no.py
+++ large_no.py
@@ -8,6 +8,7 @@ def large(a,b,c):
   if (c>=a) and (c>=b):
     return(c)
 
+st.write("Larger of the 3 numbers")
 a=st.number_input("Enter the first Number:")
 b=st.number_input("Enter the second Number:")
 c=st.number_input("Enter the third Number:")
